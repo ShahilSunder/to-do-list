713e4472bd09929b7b7706ea7ddb2170643305fb
Shahil Sunder <shahilsunder@dimensiondata.com>
2022-02-06 12:52:04 +0200

    feat: added list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ListComponent } from './list/list.component';
 // Angular Material Components
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
